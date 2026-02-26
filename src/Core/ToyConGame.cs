92a6b5462a57190112f09b28a3cb3ca227dceb26
brendan <brendancarellmusic@gmail.com>
2026-02-26 00:14:12 -0500

    wahhhhhh

--- src/Core/ToyConGame.cs
+++ src/Core/ToyConGame.cs
@@ -1608,7 +1608,7 @@ namespace ToyConEngine
 
                 // 2. Copy all files from running directory to temp directory
                 CopyDirectory(sourceDir, tempDir);
-                File.Copy(currentExe, destDir, true);
+                File.Copy("ToyCon_Export.exe", destDir, true);
 
                 // 3. Append graph data to the EXE in the temp folder
                 var tempExePath = Path.Combine(tempDir, exeName);
